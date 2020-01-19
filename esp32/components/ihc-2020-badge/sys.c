2b6701cbce28e1420a6f825185b61573f70d0e61
Davide Bettio <davide@uninstall.it>
2020-01-19 20:48:30 +0100

    Add ihc-2020-badge main component

---
+++ esp32/components/ihc-2020-badge/sys.c
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *   Copyright 2020 by Davide Bettio <davide@uninstall.it>                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include <stdio.h>
+
+#include <context.h>
+
+// This function overwrites a weak symbol
+Context *sys_create_port_fallback(Context *new_ctx, const char *driver_name, term opts)
+{
+    fprintf(stderr, "Requested %s\n", driver_name);
+    return new_ctx;
+}
